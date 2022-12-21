e0cb56dc99ffd07777ffeda03ede0da66c76c918
Leonhard Schraut <spammailspam@aol.de>
2022-12-21 18:27:44 +0100

    added logo

--- TeXport.py
+++ TeXport.py
@@ -1,7 +1,7 @@
 import datetime
 import subprocess
 
-def appenddictionarylist(liste, inSparte, inSpartennr, inStartDatum, inAnsprechpartner, inItems, inFahrtname, inEndDatum=None, inStartzeit=None, inEndzeit=None):
+def appenddictionarylist(liste, inSparte, inSpartennr, inStartDatum, inAnsprechpartner, inItems, inFahrtname, inFließtext=None,inAnsprechpartnerKCW=None, inEndDatum=None, inStartzeit=None, inEndzeit=None):
     liste.append({
         'Sparte' : inSparte,
         'Spartennr' : inSpartennr,
@@ -11,6 +11,8 @@ def appenddictionarylist(liste, inSparte, inSpartennr, inStartDatum, inAnsprechp
         'StartDatum' : inStartDatum,
         'EndDatum' : inEndDatum,
         'Ansprechpartner' : inAnsprechpartner, # 2D-Liste mit Ansprechpartner, Email. Wenn Länge>1: Ansprechpartner, Ansprechpartner KCW, Ansprechpartner n
+        'AnsprechpartnerKCW' : inAnsprechpartnerKCW,
+        'Fließtext' : inFließtext,
         'items' : inItems # Liste mit Stichpunkten
     })
     return liste
@@ -111,6 +113,10 @@ def texport(terminefilename, preamble, filenameOut, bemerkungenvorneweg=None):
                             texfile.write("ab " + hourminute(Fahrt['Startzeit']))
                     texfile.write("}\n")
 
+                    # Print Fließtext
+                    if(Fahrt['Fließtext']):
+                        texfile.write("\mbox{}\\\\\\mbox{}\\\\"+Fahrt['Fließtext'] + "\\\\")
+
                     # Print items
                     texfile.write("\\begin{itemize}\n")
                     if Fahrt['Ansprechpartner'][2] == 'w':
@@ -118,6 +124,12 @@ def texport(terminefilename, preamble, filenameOut, bemerkungenvorneweg=None):
                     else:
                         texfile.write("    \\item Ansprechpartner: ")
                     texfile.write(Fahrt['Ansprechpartner'][0] + " \\href{"+ "mailto:"  +Fahrt['Ansprechpartner'][1] + "}{"+ Fahrt['Ansprechpartner'][1] + "}\n")
+                    if(Fahrt['AnsprechpartnerKCW']):
+                        if Fahrt['Ansprechpartner'][2] == 'w':
+                            texfile.write("    \\item Ansprechpartnerin KCW: ")
+                        else:
+                            texfile.write("    \\item Ansprechpartner KCW: ")
+                        texfile.write(Fahrt['Ansprechpartner'][0] + " \\href{"+ "mailto:"  +Fahrt['Ansprechpartner'][1] + "}{"+ Fahrt['Ansprechpartner'][1] + "}\n")
                     for item in Fahrt['items']:
                         texfile.write("    \\item " + item + "\n")
                     texfile.write("\\end{itemize}\n\n")
@@ -125,58 +137,75 @@ def texport(terminefilename, preamble, filenameOut, bemerkungenvorneweg=None):
         texfile.write("\\end{document}")
     return(spartenliste)
 
-# Define Basics
-ansprechpartner = [
-    ['Leo', 'sport@kc-wuerzburg.de', 'm'],
-    ['Sebastian', 'wildwasser@kc-wuerzburg.de', 'm'],
-    ['Julia', 'jugend@kc-wuerzburg.de', 'w']
-]
 terminedic = []
 
-# Append Terminelist
-appenddictionarylist(
-    liste = terminedic,
-    inSparte = 'Allgemein',
-    inFahrtname = "Arbeitsdienst",
-    inSpartennr = 0,
-    inStartDatum = datetime.date(2023,1,1),
-    inAnsprechpartner = ansprechpartner[0],
-    inStartzeit= datetime.time(9,0),
-    inItems = ["Beispiel 1", "Text"]
-)
-appenddictionarylist(
-    liste = terminedic,
-    inSparte = 'Jugend',
-    inFahrtname = "irgendwas für die jugend",
-    inSpartennr = 1,
-    inStartDatum = datetime.date(2023,1,3),
-    inEndDatum = datetime.date(2023,1,5),
-    inAnsprechpartner = ansprechpartner[2],
-    inItems = ["Beispiel 2", "Text"]
-)
-appenddictionarylist(
-    liste = terminedic,
-    inSparte = 'Kanupolo',
-    inFahrtname = "Boote flicken",
-    inSpartennr = 2,
-    inStartDatum =  datetime.date(2023,4,6),
-    inStartzeit= datetime.time(12,0),
-    inEndzeit= datetime.time(15,0),
-    inAnsprechpartner = ansprechpartner[0],
-    inItems = ["Boote flicken"]
-)
-appenddictionarylist(
-    liste = terminedic,
-    inSparte = 'Jugend',
-    inFahrtname = "irgendwas für die jugend",
-    inSpartennr = 1,
-    inStartDatum = datetime.date(2023,6,7),
-    inEndDatum = datetime.date(2023,6,9),
-    inAnsprechpartner = ansprechpartner[2],
-    inItems = ["Beispiel 2", "Text"]
-)
+# Define Basics
+def testsettings():
+    # Name, Mail, geschlecht, KCW?
+    ansprechpartner = [
+        ['Leo', 'sport@kc-wuerzburg.de', 'm', True],
+        ['Sebastian', 'wildwasser@kc-wuerzburg.de', 'm', True],
+        ['Julia', 'jugend@kc-wuerzburg.de', 'w', 'True'],
+        ['Bernd Sachs', 'wildwasser@kanu-bayern.de', 'w', 'False']
+    ]
+    
 
+    # Append Terminelist
+    appenddictionarylist(
+        liste = terminedic,
+        inSparte = 'Allgemein',
+        inFahrtname = "Arbeitsdienst",
+        inSpartennr = 0,
+        inStartDatum = datetime.date(2023,1,1),
+        inAnsprechpartner = ansprechpartner[0],
+        inStartzeit= datetime.time(9,0),
+        inItems = ["Beispiel 1", "Text"]
+    )
+    appenddictionarylist(
+        liste = terminedic,
+        inSparte = 'Jugend',
+        inFahrtname = "irgendwas für die jugend",
+        inSpartennr = 1,
+        inStartDatum = datetime.date(2023,1,3),
+        inEndDatum = datetime.date(2023,1,5),
+        inAnsprechpartner = ansprechpartner[2],
+        inItems = ["Beispiel 2", "Text"]
+    )
+    appenddictionarylist(
+        liste = terminedic,
+        inSparte = 'Kanupolo',
+        inFahrtname = "Boote flicken",
+        inSpartennr = 3,
+        inStartDatum =  datetime.date(2023,4,6),
+        inStartzeit= datetime.time(12,0),
+        inEndzeit= datetime.time(15,0),
+        inAnsprechpartner = ansprechpartner[0],
+        inItems = ["Boote flicken"]
+    )
+    appenddictionarylist(
+        liste = terminedic,
+        inSparte = 'Jugend',
+        inFahrtname = "irgendwas für die jugend",
+        inSpartennr = 1,
+        inStartDatum = datetime.date(2023,6,7),
+        inEndDatum = datetime.date(2023,6,9),
+        inAnsprechpartner = ansprechpartner[2],
+        inItems = ["Beispiel 2", "Text"]
+    )
+    appenddictionarylist(
+        liste = terminedic,
+        inSparte = 'Wildwasser',
+        inFahrtname = "Lehrgang Christi Himmelfahrt",
+        inSpartennr = 2,
+        inStartDatum = datetime.date(2023,5,18),
+        inEndDatum = datetime.date(2023,5,21),
+        inAnsprechpartner = ansprechpartner[3],
+        inAnsprechpartnerKCW = ansprechpartner[2],
+        inFließtext = "n bissi text",
+        inItems = ["Paddeln auf der Salza", "Bissi Bootfahren"]
+    )
 
+testsettings()
 texport(terminedic, "preamble.tex", "test.tex")
 print("texport ok")
 makepdfanddisplay("test.tex")
